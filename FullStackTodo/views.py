10fa63fe35150162c734f282bf6015237435b504
Vikash1440 <meenavikash34262@gmail.com>
2025-11-23 17:01:39 +0530

    create models

--- FullStackTodo/views.py
+++ FullStackTodo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(req):
-    task=Task.objects.filter(is_completed=False)
+    task=Task.objects.filter(is_completed=False).order_by('-updated_at')
     print(task)
     return render(req,"home.html",{'tasks':task})
